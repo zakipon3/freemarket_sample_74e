43892a6a1dd3f9b587391a584f166cbe8c0a45a6
masashi-nitta <masashinitta.1102@gmail.com>
2020-05-12 19:28:45 +0900

    add days_until_shipping selectbox
    ・発送までの日数のセレクトボックスをactivehashを利用して作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,5 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
   belongs_to_active_hash :deliveryfee
+  belongs_to_active_hash :daysuntilshipping
 end
