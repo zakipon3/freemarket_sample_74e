f453611f44eb42a03cf01453154eaf09467573df
masashi-nitta <masashinitta.1102@gmail.com>
2020-05-12 19:08:52 +0900

    add deliveryfee selectbox
    ・配送料の負担のセレクトボックスをactivehashを利用して作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  
+
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
+  belongs_to_active_hash :deliveryfee
 end
