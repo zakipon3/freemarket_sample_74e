d4246e8416a38c5637da76098f78fb454a96363e
masashi-nitta <masashinitta.1102@gmail.com>
2020-05-13 17:24:19 +0900

    change activehash difinition
    ・activehashの定義場所が誤っていたので修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,2 @@
 class Address < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :deliveryfee
-  belongs_to_active_hash :daysuntilshipping
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :deliveryfee
+  belongs_to_active_hash :daysuntilshipping
+
   has_many :images
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
