0d9e47d2fb8e91bb7dfa6d8de92ee0bece8e2a00
masashi-nitta <masashinitta.1102@gmail.com>
2020-05-15 10:04:16 +0900

    add categories_table_data
    ・categoriesテーブルに登録するデータを定義
    ・アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
+  belongs_to :category
+
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
   belongs_to_active_hash :deliveryfee
