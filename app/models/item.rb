79e55d7d587820892beb9fd2f9933b7777eb0f11
masashi-nitta <masashinitta.1102@gmail.com>
2020-05-17 11:00:17 +0900

    edit item_model
    ・商品データ削除時に、紐づいている画像データも一緒に削除するための追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :deliveryfee
   belongs_to_active_hash :daysuntilshipping
 
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 end
