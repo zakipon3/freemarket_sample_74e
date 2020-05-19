bf6eff667f0206b182e7d8cfdffef1c7537245f6
sayakakaka <sayaka.1228@icloud.com>
2020-05-19 22:09:43 +0900

    トップビュー表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,17 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+    # .older(created_at: :desc)
   end
 
   def new
+    # @new = New.all
   end
 
   def create
+  end
+
+  def show
   end
 
   def detail
