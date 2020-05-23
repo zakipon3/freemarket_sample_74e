b2bbb63d0b7c4b3f4dd763d23013a12fdcafe816
masashi-nitta <masashinitta.1102@gmail.com>
2020-05-23 10:12:02 +0900

    edit category
    ・商品編集画面でのカテゴリ取得に対応

--- app/assets/javascripts/items_category.js
+++ app/assets/javascripts/items_category.js
@@ -1,10 +1,11 @@
 $(function(){
   var request = $("#request").attr("action");
-  if(request.indexOf("new") != -1 || request.indexOf("edit") != -1){
+  // if(request.indexOf("new") != -1 || request.indexOf("edit") != -1){
+  if(request.indexOf("new") != -1){
     $.ajax({
       url: "/items/set_parents"
     }).done(function(data){
-      $("#category-select").append(`<select class="exhibit__wrapper__itemdetail__selectbox select-parent" name="item[category_id]" id="product_category_id"><option value="">選択してください</option></select>`);
+      $("#category-select").append(`<select class="exhibit__wrapper__itemdetail__selectbox select-parent" name="item[category_id]" id="item_category_id"><option value="">選択してください</option></select>`);
       data.parents.forEach(function(parent){
         $(".select-parent").append(`<option value="${parent.id}">${parent.name}</option>`);
       })
@@ -47,6 +48,62 @@ $(function(){
             $(".select-child").attr("name", "select-parent");
             $(".select-child").css("margin-bottom", "10px");
             $("#category-select").append(`<select class="exhibit__wrapper__itemdetail__selectbox select-grandchild" name="item[category_id]" id="item_category_id"><option value="">選択してください</option></select>`);
+            data.grandchildren.forEach(function(grandchild){
+              $(".select-grandchild").append(`<option value="${grandchild.id}">${grandchild.name}</option>`);
+            })
+          })
+        }
+      })
+    })
+  } else if(request.indexOf("edit") != -1){
+    $.ajax({
+      url: "/items/set_parents"
+    }).done(function(data){
+      // console.log("11");
+      // $("#category-edit").append(`<select class="exhibit__wrapper__itemdetail__selectbox select-parent" name="item[category_id]" id="item_category_id"><option value="">選択してください</option></select>`);
+      // data.parents.forEach(function(parent){
+      //   $(".select-parent").append(`<option value="${parent.id}">${parent.name}</option>`);
+      // })
+      $(".select-parent").on("change", function(){
+        $(".select-child").remove();
+        $(".select-grandchild").remove();
+        if($(this).val() == ""){
+          $(".select-parent").attr("id"  , "item_category_id");
+          $(".select-parent").attr("name", "item[category_id]");
+          $(".select-parent").css("margin-bottom", "0");
+        }else{
+          $.ajax({
+            url     : "/items/set_children",
+            data    : {parent_id: $(this).val()},
+            dataType: "json"
+          }).done(function(data){
+            $(".select-parent").attr("id"  , "select-parent");
+            $(".select-parent").attr("name", "select-parent");
+            $(".select-parent").css("margin-bottom", "10px");
+            $("#category-edit").append(`<select class="exhibit__wrapper__itemdetail__selectbox select-child" name="item[category_id]" id="item_category_id"><option value="">選択してください</option></select>`);
+            data.children.forEach(function(child){
+              $(".select-child").append(`<option value="${child.id}">${child.name}</option>`);
+            })
+          })
+        }
+      })
+      $("#category-edit").on("change", ".select-child", function(){
+        $(".select-grandchild").remove();
+        if($(this).val() == ""){
+          console.log("6");
+          $(".select-child").attr("id"  , "item_category_id");
+          $(".select-child").attr("name", "item[category_id]");
+          $(".select-child").css("margin-bottom", "0");
+        }else{
+          $.ajax({
+            url     : "/items/set_grandchildren",
+            data    : {ancestry: `${$(".select-parent").val()}/${$(this).val()}`},
+            dataType: "json"
+          }).done(function(data){
+            $(".select-child").attr("id"  , "select-parent");
+            $(".select-child").attr("name", "select-parent");
+            $(".select-child").css("margin-bottom", "10px");
+            $("#category-edit").append(`<select class="exhibit__wrapper__itemdetail__selectbox select-grandchild" name="item[category_id]" id="item_category_id"><option value="">選択してください</option></select>`);
             data.grandchildren.forEach(function(grandchild){
               $(".select-grandchild").append(`<option value="${grandchild.id}">${grandchild.name}</option>`);
             })
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_params, only: :create
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
   end
@@ -21,6 +22,33 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    grandchild_category = @item.category
+    child_category = grandchild_category.parent
+
+    @category = Category.where(ancestry: nil)
+
+    @category_children_array = []
+    Category.where(ancestry: child_category.ancestry).each do |children|
+      @category_children_array << children
+    end
+
+    @category_grandchildren_array = []
+    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
+      @category_grandchildren_array << grandchildren
+    end
+  end
+
+  def update
+    if @item.valid?
+      unless @item.update(set_params)
+        redirect_to edit_item_path, flash: { error: @item.errors.full_messages }
+      end
+    else
+      redirect_to new_item_path, flash: { error: @item.errors.full_messages }
+    end
+  end
+
   def set_parents
     @parents  = Category.where(ancestry: nil)
   end
@@ -33,9 +61,17 @@ class ItemsController < ApplicationController
     @grandchildren = Category.where(ancestry: params[:ancestry])
   end
 
+  def set_images
+    @images = Image.where(item_id: params[:id])
+  end
+
   private
   def set_params
-    params.require(:item).permit(:name, :explanation, :category_id, :size, :brand_name, :condition_id, :delivery_fee_id, :prefecture_id, :days_until_shipping_id, :price, images_attributes: [:image, :_destroy, :id]).merge(seller_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :category_id, :size, :brand_name, :condition_id, :delivery_fee_id, :prefecture_id, :days_until_shipping_id, :price, images_attributes: [:image, :id, :_destroy]).merge(seller_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def detail
