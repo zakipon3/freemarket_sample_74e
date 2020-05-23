acd4494acc011449e35632c6d50bc9cba1af17a2
zakipon3 <miyapon3@gmail.com>
2020-05-23 21:15:26 +0900

    登録カードの表示と削除・再登録

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -1,14 +1,15 @@
 class CreditcardsController < ApplicationController
   require "payjp"
+  before_action :set_card
 
   def index
     # すでにクレジットカードが登録しているか？
     if @card.present?
       # 登録している場合,PAY.JPからカード情報を取得する
       # PAY.JPの秘密鍵をセットする。
-      Payjp.api_key = ENV[:PAYJP_PRIVATE_KEY]
+      Payjp.api_key = Rails.application.credentials.pay_jp[:payjp_private_key]
       # PAY.JPから顧客情報を取得する。
-      customer = Payjp::Customer.retrieve(@card.payjp_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       # PAY.JPの顧客情報から、デフォルトで使うクレジットカードを取得する。
       @card_info = customer.cards.retrieve(customer.default_card)
       # クレジットカード情報から表示させたい情報を定義する。
@@ -66,9 +67,9 @@ class CreditcardsController < ApplicationController
   def destroy     
     # 今回はクレジットカードを削除するだけでなく、PAY.JPの顧客情報も削除する。これによりcreateメソッドが複雑にならない。
     # PAY.JPの秘密鍵をセットして、PAY.JPから情報をする。
-    Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
+    Payjp.api_key = Rails.application.credentials.pay_jp[:payjp_private_key]
     # PAY.JPの顧客情報を取得
-    customer = Payjp::Customer.retrieve(@card.payjp_id)
+    customer = Payjp::Customer.retrieve(@card.customer_id)
     customer.delete # PAY.JPの顧客情報を削除
     if @card.destroy # App上でもクレジットカードを削除
       redirect_to action: "index", notice: "削除しました"
@@ -82,54 +83,3 @@ class CreditcardsController < ApplicationController
     @card = Creditcard.where(user_id: current_user.id).first if Creditcard.where(user_id: current_user.id).present?
   end
 end
-
-
-#   def new
-#     card = Creditcard.where(user_id: current_user.id)
-#     redirect_to index_path(current_user.id) if card.exists?
-#   end
-
-
-#   def pay #payjpとCardのデータベース作成
-#     Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
-#     #保管した顧客IDでpayjpから情報取得
-#     if params['payjp-token'].blank?
-#       redirect_to new_card_path
-#     else
-#       customer = Payjp::Customer.create(
-#         card: params['payjp-token'],
-#         metadata: {user_id: current_user.id}
-#       ) 
-#       @card = Creditcard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-#       if @card.save
-#         redirect_to card_path(current_user.id)
-#       else
-#         redirect_to pay_cards_path
-#       end
-#     end
-#   end
-
-#   def destroy #PayjpとCardデータベースを削除
-#     card = Creditcard.find_by(user_id: current_user.id)
-#     if card.blank?
-#     else
-#       Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
-#       customer = Payjp::Customer.retrieve(card.customer_id)
-#       customer.delete
-#       card.delete
-#     end
-#       redirect_to new_card_path
-#   end
-
-#   def show #Cardのデータpayjpに送り情報を取り出す
-#     card = Creditcard.find_by(user_id: current_user.id)
-#     if card.blank?
-#       redirect_to new_card_path 
-#     else
-#       Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
-#       customer = Payjp::Customer.retrieve(card.customer_id)
-#       @default_card_information = customer.cards.retrieve(card.card_id)
-#     end
-#   end
-
-
