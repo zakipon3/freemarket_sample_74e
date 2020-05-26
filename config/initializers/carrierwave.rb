523b54f51b376d19a2d9fe994e9a804fc9a55907
zakipon3 <miyapon3@gmail.com>
2020-05-26 16:38:02 +0900

    end抜け修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,15 +6,15 @@ CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
   else
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'freemarketsample74e'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample74e'
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'freemarketsample74e'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample74e'
+  end
 end
